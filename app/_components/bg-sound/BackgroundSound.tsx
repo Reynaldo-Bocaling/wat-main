d2e99e8913310deeb7c5b4c16d732013187aa24a
Reynaldo <reynaldobocaling@gmail.com>
2024-08-21 20:59:36 +0800

    update

--- app/_components/bg-sound/BackgroundSound.tsx
+++ app/_components/bg-sound/BackgroundSound.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const BackgroundSound: React.FC = () => {
-  return <audio src="/bgsound.mp3" autoPlay loop></audio>;
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    const playAudio = async () => {
+      if (audioRef.current) {
+        try {
+          await audioRef.current.play();
+        } catch (error) {
+          console.error("Audio playback failed:", error);
+        }
+      }
+    };
+
+    playAudio();
+  }, []); // Empty dependency array, para tumakbo lang isang beses sa initial render
+
+  return (
+    <audio ref={audioRef} src="/bgsound.mp3" loop muted>
+      Your browser does not support the <code>audio</code> element.
+    </audio>
+  );
 };
 
 export default BackgroundSound;
